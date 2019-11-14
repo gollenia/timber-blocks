dcf737c95ef06c3b7651d28e8d6d2d601e7613ba
www-data <www-data@alpine.localdomain>
2019-11-14 08:01:31 +0100

    Bugfix an Name Update

--- plugin.php
+++ plugin.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Plugin Name: contexis-bootstrap — CGB Gutenberg Block Plugin
+ * Plugin Name: CTX Blocks for Bootstrap
  * Plugin URI: https://github.com/ahmadawais/create-guten-block/
  * Description: contexis-bootstrap — is a Gutenberg plugin created via create-guten-block.
- * Author: mrahmadawais, maedahbatool
+ * Author: mrahmadawais, maedahbatool, thomasgollenia
  * Author URI: https://AhmadAwais.com/
  * Version: 1.0.0
  * License: GPL2+
