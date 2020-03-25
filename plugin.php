da7dbb44192f7a2be542b8b984055fa17c7d0308
Thomas Gollenia <thomas@gollenia.at>
2020-03-25 14:24:39 +0100

    Plugin Info updated

--- plugin.php
+++ plugin.php
@@ -1,15 +1,15 @@
 <?php
 /**
- * Plugin Name: CTX Blocks for Bootstrap
- * Plugin URI: https://github.com/ahmadawais/create-guten-block/
- * Description: contexis-bootstrap — is a Gutenberg plugin created via create-guten-block.
- * Author: mrahmadawais, maedahbatool, thomasgollenia
- * Author URI: https://AhmadAwais.com/
+ * Plugin Name: CTX Blocks for UiKit
+ * Plugin URI: https://github.com/gollenia/ctx-blocks/
+ * Description: Use Uikit-Features in Gutenberg Block Editor
+ * Author: Thomas Gollenia
+ * Author URI: https://www.gollenia.at
  * Version: 1.0.0
  * License: GPL2+
  * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
  *
- * @package CGB
+ * @package CTX
  */
 
 // Exit if accessed directly.
