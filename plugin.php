bf7ad4c72868ca620f892e886ae3c9333fb477d9
Thomas Gollenia <thomas@gollenia.at>
2020-04-06 11:13:21 +0200

    Version update to 1.0.0

--- plugin.php
+++ plugin.php
@@ -5,7 +5,7 @@
  * Description: Use Uikit-Features in Gutenberg Block Editor
  * Author: Thomas Gollenia
  * Author URI: https://www.gollenia.at
- * Version: 0.0.8
+ * Version: 1.0.0
  * License: MIT
  * License URI: https://opensource.org/licenses/MIT
  * @package CTX
