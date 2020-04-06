5bac7d344ee113c079adae9f940ad66aa83cd2ae
Thomas Gollenia <thomas@gollenia.at>
2020-04-06 11:05:50 +0200

    New Icon

--- src/blocks/grid/grid-row/icons.js
+++ src/blocks/grid/grid-row/icons.js
@@ -2,7 +2,7 @@ import { Path, SVG } from '@wordpress/components';
 
 var icons = {};
 
-icons.row = <SVG xmlns="http://www.w3.org/2000/SVG" height="24" viewBox="0 0 24 24" width="24"><Path d="M10 18h5V5h-5v13zm-6 0h5V5H4v13zM16 5v13h5V5h-5z"/><Path d="M0 0h24v24H0z" fill="none"/></SVG>
+icons.row = <SVG xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><Path d="M22 13h-8v-2h8v2zm0-6h-8v2h8V7zm-8 10h8v-2h-8v2zm-2-8v6c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V9c0-1.1.9-2 2-2h6c1.1 0 2 .9 2 2zm-1.5 6l-2.25-3-1.75 2.26-1.25-1.51L3.5 15h7z"/></SVG>
 icons.parallax = <SVG xmlns="http://www.w3.org/2000/SVG" height="24" viewBox="0 0 24 24" width="24"><Path d="M10 18h4v-2h-4v2zM3 6v2h18V6H3zm3 7h12v-2H6v2z"/><Path d="M0 0h24v24H0z" fill="none"/></SVG> 
 icons.masnory = <SVG xmlns="http://www.w3.org/2000/SVG" height="24" viewBox="0 0 24 24" width="24"><Path d="M0 0h24v24H0z" fill="none"/><Path d="M3 13h8V3H3v10zm0 8h8v-6H3v6zm10 0h8V11h-8v10zm0-18v6h8V3h-8z"/></SVG>
 icons.equalizer = <SVG xmlns="http://www.w3.org/2000/SVG" height="24" viewBox="0 0 24 24" width="24"><Path d="M4 11h5V5H4v6zm0 7h5v-6H4v6zm6 0h5v-6h-5v6zm6 0h5v-6h-5v6zm-6-7h5V5h-5v6zm6-6v6h5V5h-5z"/><Path d="M0 0h24v24H0z" fill="none"/></SVG>
