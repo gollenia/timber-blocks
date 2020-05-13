a26dfd76cd26485acb9b1fbfd1dc8c18ce237203
Thomas Gollenia <thomas@gollenia.at>
2020-05-13 08:18:41 +0200

    Reordered Code into Classes

--- plugin.php
+++ plugin.php
@@ -16,7 +16,31 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-/**
- * Block Initializer.
- */
-require_once plugin_dir_path( __FILE__ ) . 'src/init.php';
+require_once plugin_dir_path( __FILE__ ) . 'lib/classes/Assets.php';
+require_once plugin_dir_path( __FILE__ ) . 'lib/classes/ImageSizes.php';
+require_once plugin_dir_path( __FILE__ ) . 'lib/classes/Block.php';
+
+Contexis\Blocks\ImageSizes::register();
+$args = Contexis\Blocks\Assets::register();
+
+
+$blocks = [
+	'ctx-blocks/accordion-collection',
+	'ctx-blocks/accordion-item',
+	'ctx-blocks/description-list',
+	'ctx-blocks/description-item',
+	'ctx-blocks/button',
+	'ctx-blocks/card',
+	'ctx-blocks/progress',
+	'ctx-blocks/modal',
+	'ctx-blocks/columns',
+	'ctx-blocks/alert',
+	'ctx-blocks/section',
+	'ctx-blocks/grid-row',
+	'ctx-blocks/grid-column',
+	'ctx-blocks/image',
+];
+
+Contexis\Blocks\Block::registerSimpleBlocks($blocks, $args);
+Contexis\Blocks\Block::registerDynamicBlocks($args);
+
