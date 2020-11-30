5b53bfbb1b6b6cdc83365aaba2f78e86951b9671
Thomas Gollenia <thomas@gollenia.at>
2020-11-30 15:42:00 +0100

    *star* Auto Flow feature added

--- src/blocks/grid/grid-row/inspector.js
+++ src/blocks/grid/grid-row/inspector.js
@@ -4,7 +4,7 @@
 import { __ } from '@wordpress/i18n';
 import { Component, Fragment } from '@wordpress/element';
 import { InspectorControls } from '@wordpress/block-editor';
-import { RangeControl, CheckboxControl, PanelBody } from '@wordpress/components';
+import { RangeControl, CheckboxControl, PanelBody, SelectControl, PanelRow } from '@wordpress/components';
 
 /**
  * Inspector controls
@@ -20,6 +20,10 @@ class Inspector extends Component {
 		const {
 			gapSize,
             equalizer,
+            autoFlow,
+            autoFlowDense,
+            autoFlowRows,
+            autoFlowDirection,
             childrenWidthLarge,
             childrenWidthMedium
         } = attributes;
@@ -59,7 +63,42 @@ class Inspector extends Component {
                             checked={equalizer}
                             onChange={(value) => {setAttributes( { equalizer: value })}}
                         />
+                         
 
+                    </PanelBody>
+                    <PanelBody
+                        title="Auto Flow"
+                        initialOpen={false}
+                    >
+                        <CheckboxControl
+                            label="Auto Flow aktivieren"
+                            checked={autoFlow}
+                            onChange={(value) => {setAttributes( { autoFlow: value })}}
+                        />
+                        <SelectControl
+                            label='Anordnung'
+                            value={ autoFlowDirection }
+                            disabled={!autoFlow}
+                            options={ [
+                                { label: 'Reihen', value: 'row' },
+                                { label: 'Spalten', value: 'col' },
+                            ] }
+                            onChange={ ( event ) => { setAttributes( { autoFlowDirection: event } ) } }
+                        />
+                        <CheckboxControl
+                            label="Abstände minimieren"
+                            checked={autoFlowDense}
+                            disabled={!autoFlow}
+                            onChange={(value) => {setAttributes( { autoFlowDense: value })}}
+                        />
+                        <RangeControl  
+                            label="Anzahl der Reihen"
+                            max={ 12 }
+                            min={ 1 }
+                            disabled={!autoFlow && autoFlowDirection == 'col'}
+                            onChange={(value) => {setAttributes( { autoFlowRows: value })}}
+                            value={ autoFlowRows }
+                        />
                     </PanelBody>
                 </InspectorControls>
 			</Fragment>
