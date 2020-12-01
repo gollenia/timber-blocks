1d1289fa9f0e43b231f8560c4f499a7150993b0e
Thomas Gollenia <thomas@gollenia.at>
2020-12-01 09:31:27 +0100

    :bug: InnerBlocks import

--- src/blocks/accordion/accordion-item/index.js
+++ src/blocks/accordion/accordion-item/index.js
@@ -4,6 +4,7 @@
 import edit from './edit';
 import icon from './icon';
 import metadata from './block.json';
+import { InnerBlocks, RichText } from '@wordpress/block-editor';
 import './editor.scss';
 
 /**
