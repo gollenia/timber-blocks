1bdc59dac87051621a5078f2149da23fd7ee9a7e
Thomas Gollenia <thomas@gollenia.at>
2021-02-12 09:56:38 +0100

    Translation

--- src/blocks/image/edit.js
+++ src/blocks/image/edit.js
@@ -79,35 +79,35 @@ export default class ImageEdit extends Component {
 									<Fragment>
 										<div class="components-placeholder__label"><span class="block-editor-block-icon"><svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" role="img" aria-hidden="true" focusable="false"><path d="m19 5v14h-14v-14h14m0-2h-14c-1.1 0-2 0.9-2 2v14c0 1.1 0.9 2 2 2h14c1.1 0 2-0.9 2-2v-14c0-1.1-0.9-2-2-2z"></path><path d="m14.14 11.86l-3 3.87-2.14-2.59-3 3.86h12l-3.86-5.14z"></path></svg></span>Bild</div>
 										<div class="components-placeholder__instructions">
-											Wähle ein Bild aus der Mediathek
+										{__("Select image from library", 'ctx-blocks')}
 										</div>
-										<button type="button" className="components-button is-secondary" onClick={ open }>Mediathek</button>
+										<button type="button" className="components-button is-secondary" onClick={ open }>{__("Media library", 'ctx-blocks')}</button>
 									</Fragment>
 								}
 								{ image && 
 									<Fragment>
 										<div className={imageHolderClass}>
 											{ round && image.sizes.qmedium &&
-												<img width={`${width}%`} className={imageClasses} src={image.sizes.qmedium.url} alt="Kein Bild geladen"/> 
+												<img width={`${width}%`} className={imageClasses} src={image.sizes.qmedium.url} alt={__("No image loaded", 'ctx-blocks')}/> 
 											}
 											{ round && !image.sizes.qmedium &&
-												<p>Fehler: Das Bild hat nicht die erforderlichen Maße. Bitte laden Sie es erneut auf den Server.</p> 
+												<p>{__("The image is too small to be rendered. Please upload a larger image", 'ctx-blocks')}</p> 
 											}
-											{ !round && <img width={`${width}%`} className={imageClasses} src={image.url} alt="Kein Bild geladen"/> }
+											{ !round && <img width={`${width}%`} className={imageClasses} src={image.url} alt={__("No image loaded", 'ctx-blocks')} /> }
 											{hasOverlay && 
 												<div style={style} className="ctx-overlay">
 													<RichText
 														tagName="div"
-														label={__("Text", 'ctxblocks')}
+														label={__("Text", 'ctx-blocks')}
 														value={ overlayText }
 														onChange={ (value) => setAttributes({ overlayText: value }) }
-														placeholder={__("Text für Overlay", 'ctxblocks')}
+														placeholder={__("Write text here...", 'ctx-blocks')}
 														keepPlaceholderOnFocus={true}
 													/>
 												</div>
 											}
 										</div> 
-										<button type="button" className="components-button is-secondary" onClick={ open }>Bild ersetzen</button>
+										<button type="button" className="components-button is-secondary" onClick={ open }>{__("Replace image", 'ctx-blocks')}</button>
 									</Fragment>
 								}
 							</div> ;
