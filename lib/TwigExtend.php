d895c5d5cdda8d13c5cea76aad9f82aff0c80551
Thomas Gollenia <thomas@gollenia.at>
2021-02-23 15:23:20 +0100

    Twig Color Function

---
+++ lib/TwigExtend.php
@@ -0,0 +1,28 @@
+<?php
+namespace Contexis\Utils;
+
+class TwigExtend {
+
+    public static function add_to_twig($twig) {
+        $twig->addFunction( new \Timber\Twig_Function( 'get_color_by_slug', [__CLASS__,'get_color_by_slug'] ) );
+        return $twig;
+    }
+
+    public static function get_color_by_slug($slug) {
+        $color = [];
+        
+        $colors = get_theme_support('editor-color-palette');
+        //var_dump($colors);
+        foreach ($colors[0] as $set) {
+            //var_dump($set);
+            if($set['slug'] === $slug) {
+                $color = $set;
+                break;
+            }
+        }
+
+        
+
+        return $color;
+    }
+}
