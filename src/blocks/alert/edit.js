b5b72d4c8893caf48e43013d00e7129fb6d13d98
Thomas Gollenia <thomas@gollenia.at>
2021-05-07 10:13:06 +0200

    function rename for debug

--- src/blocks/alert/edit.js
+++ src/blocks/alert/edit.js
@@ -10,7 +10,7 @@ import { __ } from '@wordpress/i18n';
 import { useBlockProps, RichText} from '@wordpress/block-editor';
 
 
-export default function Edit({...props}) {
+export default function AlertEdit({...props}) {
 
 		const {
 			attributes: {
