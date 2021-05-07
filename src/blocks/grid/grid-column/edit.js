2d2547cadf4f18c7b52a4df4d3947f5db23a8d7f
Thomas Gollenia <thomas@gollenia.at>
2021-05-07 10:15:58 +0200

    renamed function

--- src/blocks/grid/grid-column/edit.js
+++ src/blocks/grid/grid-column/edit.js
@@ -4,7 +4,7 @@ import { __ } from '@wordpress/i18n';
 import { useBlockProps, InnerBlocks} from '@wordpress/block-editor';
 
 
-export default function Edit ({...props}) {
+export default function GridColumnEdit ({...props}) {
 
 		const {
 			attributes: { widthLarge },
