c509c44f9b1943334f288c3d9b18f211fea8ac0b
Thomas Gollenia <thomas@gollenia.at>
2021-05-07 10:10:38 +0200

    New hash

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '4b03e547fe956f8593faace0706d25b0');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'cf88be17347cbec9a32de8b6d9eeed78');
