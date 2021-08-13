d324cc1e0919574107de1a84726b800cbb581671
Thomas Gollenia <thomas@gollenia.at>
2021-08-13 12:55:34 +0200

    was still usefull

--- lib/Blocks/Nav.php
+++ lib/Blocks/Nav.php
@@ -19,5 +19,40 @@ class Nav extends Block {
     }
 
 
+    private function get_posts($attributes) { 
+        
+        $args = array(
+            'post_type' => 'post',
+            'cat' => $attributes['parentCategory'],
+            'posts_per_page' => $attributes['limit'],
+            'orderby' => $attributes['orderBy'],
+            'order'   => $attributes['order']
+        );
+        return Timber::get_posts( $args );
+    }
+
+
+    private function get_pages($attributes) {
+        
+        $args = array(
+            'post_type' => 'page',
+            'post_parent' => $attributes['parentPage'],
+            'posts_per_page' => $attributes['limit']
+        );
+        return Timber::get_posts( $args );
+        
+    }
+
+    private function get_categories($attributes) {
+        $args = array(
+            'post_type' => 'category',
+            //'post_parent' => $parent
+        );
+        
+        return get_categories( array('child_of' => $attributes['parentCategory']) );
+        
+    }
+
+
 
 }
