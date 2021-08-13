00deedd3d7d096d5765d2f8d62ee0a9fb44328a7
Thomas Gollenia <thomas@gollenia.at>
2021-08-13 12:34:55 +0200

    removed old stuff

--- lib/Blocks/Nav.php
+++ lib/Blocks/Nav.php
@@ -2,8 +2,7 @@
 namespace Contexis\Blocks;
 
 use \Contexis\Blocks\Block;
-use WP_Query;
-
+use Timber\Timber;
 
 class Nav extends Block {
 
@@ -12,52 +11,10 @@ class Nav extends Block {
     ];
 
     public function render($attributes, $content, $full_data) : string {
-       
         $get_data = "get_" . $attributes['dataType'];
-        
         $attributes['data'] = $this->{$get_data}($attributes);
-
         $template = $this->get_template($full_data->name);
-        
-        return \Timber\Timber::compile($template, $attributes);
-        
-    }
-
-
-    private function get_posts($attributes) { 
-        
-        $args = array(
-            'post_type' => 'post',
-            'cat' => $attributes['parentCategory'],
-            'posts_per_page' => $attributes['limit'],
-            'orderby' => $attributes['orderBy'],
-            'order'   => $attributes['order']
-        );
-        $query = new WP_Query($args);
-        return $query->posts;
-    }
-
-
-    private function get_pages($attributes) {
-        
-        $args = array(
-            'post_type' => 'page',
-            'post_parent' => $attributes['parentPage'],
-            'posts_per_page' => $attributes['limit']
-        );
-        $query = new WP_Query($args);
-
-        return $query->posts;
-        
-    }
-
-    private function get_categories($attributes) {
-        $args = array(
-            'post_type' => 'category',
-            //'post_parent' => $parent
-        );
-        
-        return get_categories( array('child_of' => $attributes['parentCategory']) );
+        return Timber::compile($template, $attributes);
         
     }
 
