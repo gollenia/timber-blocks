972d73279310c56026d4db03e367167ef12ad448
Thomas Gollenia <thomas@gollenia.at>
2021-11-02 10:22:17 +0100

    new paths

--- lib/Assets.php
+++ lib/Assets.php
@@ -9,13 +9,14 @@ class Assets {
         add_action( 'init', function() {
             $dir = __DIR__;
 
-            $script_asset_path = "$dir/../build/index.asset.php";
+
+            $script_asset_path = "$dir/../assets/index.asset.php";
             if ( ! file_exists( $script_asset_path ) ) {
                   throw new \Error(
                           'You need to run `npm start` or `npm run build` for the "create-block/ctx-blocks" block first.'
                  );
             }
-            $index_js     = '../build/index.js';
+            $index_js     = '../assets/index.js';
             $script_asset = require( $script_asset_path );
             wp_register_script(
                   'create-block-ctx-blocks-block-editor',
@@ -25,7 +26,7 @@ class Assets {
             );
             wp_set_script_translations( 'create-block-ctx-blocks-block-editor', 'ctx-blocks', plugin_dir_path( __FILE__ ) . '../languages' );
             
-            $editor_css = '../build/index.css';
+            $editor_css = '../assets/index.css';
 
             wp_register_style(
                      'create-block-ctx-blocks-block-editor',
@@ -34,7 +35,7 @@ class Assets {
                      filemtime( "$dir/$editor_css" )
             );
     
-             $style_css = '../build/style-index.css';
+             $style_css = '../assets/style-index.css';
              wp_register_style(
                      'create-block-ctx-blocks-block',
                      plugins_url( $style_css, __FILE__ ),
@@ -45,7 +46,13 @@ class Assets {
             
         } );
 
-       
+       add_action('admin_enqueue_scripts', function() {
+            wp_enqueue_script('ctx-block-filter', plugin_dir_url(__FILE__) . "../assets/admin.js", [], false, true);
+       });
+
+       add_action('init', function() {
+        wp_enqueue_script('ctx-frontend', plugin_dir_url(__FILE__) . "../assets/frontend.js", [], false, true);
+        });
 
         return [
             'style'         => 'create-block-ctx-blocks-block',
@@ -54,6 +61,8 @@ class Assets {
         ];
     
     }
+
+    
 
    
 }
