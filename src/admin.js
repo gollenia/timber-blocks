2784d896159b6d76eda017c06686dc2d464d984d
Thomas Gollenia <thomas@gollenia.at>
2021-12-12 16:43:10 +0100

    core/classic removed

--- src/admin.js
+++ src/admin.js
@@ -1,11 +1,9 @@
 import { unregisterBlockType } from '@wordpress/blocks';
-import domReady from '@wordpress/dom-ready';
 
 document.addEventListener('DOMContentLoaded', function () {
-    unregisterBlockType('core/columns');
-    unregisterBlockType('core/buttons');
-    unregisterBlockType('core/classic');
-    unregisterBlockType('core/image');
-    unregisterBlockType('core/more');
-    unregisterBlockType('core/nextpage');
+	unregisterBlockType('core/columns');
+	unregisterBlockType('core/buttons');
+	unregisterBlockType('core/image');
+	unregisterBlockType('core/more');
+	unregisterBlockType('core/nextpage');
 })
