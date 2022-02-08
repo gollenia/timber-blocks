f5a4356bb7e3acaa0635cc0dfab591077fa92200
Thomas Gollenia <thomas@gollenia.at>
2022-02-08 14:52:12 +0100

    new build

--- assets/index.asset.php
+++ assets/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => '9f02def17cb6843f6d63263b3e78f127');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => '60d607a17f1958a2acbd72a6e1785bcb');
