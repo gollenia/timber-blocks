2a473ccf7bd14b46cd18f636a15c077375b2a3ec
Thomas Gollenia <thomas@gollenia.at>
2022-02-08 11:39:38 +0100

    now works!

--- src/mods/baseBlocksClasses.js
+++ src/mods/baseBlocksClasses.js
@@ -2,9 +2,13 @@ const { addFilter } = wp.hooks;
 
 const setExtraPropsToBlockType = (props, blockType, attributes) => {
     const notDefined = (typeof props.className === 'undefined' || !props.className) ? true : false
-    return Object.assign(props, {
+	
+    let obj = Object.assign(props, {
         className: notDefined ? `core-block` : `core-block ${props.className}`,
     });
+	return obj;
+
 };
 
-addFilter( 'blocks.registerBlockType', 'ctx-blocks/core-visibility', setExtraPropsToBlockType );
+addFilter( 'blocks.registerBlockType', 'ctx-blocks/core-class', setExtraPropsToBlockType );
+addFilter( 'blocks.getSaveContent.extraProps', 'ctx-blocks/core-class', setExtraPropsToBlockType );
