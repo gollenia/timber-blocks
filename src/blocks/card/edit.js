25cf35c9a05cfe26636e7e6d64b97227058d4a34
Thomas Gollenia <thomas@gollenia.at>
2022-02-09 11:11:43 +0100

    added hr to allowed blocks

--- src/blocks/card/edit.js
+++ src/blocks/card/edit.js
@@ -5,7 +5,7 @@ import { __ } from '@wordpress/i18n';
 import { useBlockProps, InnerBlocks} from '@wordpress/block-editor';
 import { colord } from 'colord';
 
-const ALLOWED_BLOCKS = ['core/spacer', 'core/paragraph', 'core/shortcode', 'core/heading', 'core/list', 'ctx-blocks/button-group', 'ctx-blocks/button',  'ctx-blocks/image', 'ctx-blocks/nav', 'ctx-blocks/posts', 'ctx-blocks/grid-row', 'ctx-blocks/description-list', 'ctx-blocks/accordion-collection', 'ctx-blocks/modal', 'ctx-blocks/progress']
+const ALLOWED_BLOCKS = ['core/spacer', 'core/separator', 'core/paragraph', 'core/shortcode', 'core/heading', 'core/list', 'ctx-blocks/button-group', 'ctx-blocks/button',  'ctx-blocks/image', 'ctx-blocks/nav', 'ctx-blocks/posts', 'ctx-blocks/grid-row', 'ctx-blocks/description-list', 'ctx-blocks/accordion-collection', 'ctx-blocks/modal', 'ctx-blocks/progress']
 
 export default function CardEdit({...props}) {
 
