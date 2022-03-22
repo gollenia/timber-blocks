a3715331bb5a43b2ecd508951d70c572b244e603
Thomas Gollenia <thomas@gollenia.at>
2022-03-22 08:10:37 +0100

    URL added

--- src/blocks/image/inspector.js
+++ src/blocks/image/inspector.js
@@ -2,9 +2,8 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { Component, Fragment } from '@wordpress/element';
-import { InspectorControls } from '@wordpress/block-editor';
-import { PanelBody, CheckboxControl, RangeControl } from '@wordpress/components';
+import { InspectorControls, URLInput } from '@wordpress/block-editor';
+import { BaseControl, PanelBody, CheckboxControl, RangeControl, SelectControl } from '@wordpress/components';
 
 
 /**
@@ -12,20 +11,19 @@ import { PanelBody, CheckboxControl, RangeControl } from '@wordpress/components'
  */
 const Inspector = (props) => {
 	const {
-		attributes,
+		attributes: {
+			hasLightbox,
+			round,
+			border,
+			width,
+			shadow,
+			flip,
+			roundCorners,
+			url,
+			image
+		},
 		setAttributes,
 	} = props;
-
-	const {
-		hasLightbox,
-		round,
-		border,
-		width,
-		shadow,
-		flip,
-		roundCorners,
-	} = attributes;
-
 
 	return (
 		<>
@@ -40,44 +38,63 @@ const Inspector = (props) => {
 						onChange={ ( value ) => setAttributes( { width: value } ) }
 						min={ 0 }
 						max={ 100 }
+						step={ 10 }
 					/>
-					
+					{ image.subtype == 'svg+xml' && <p className="ctx:image__inspector-warning">
+					<span>{__('Settings not available for SVG images', 'ctx-blocks')}</span>
+					</p> }
 					<CheckboxControl
-							label={__('Circular Image', 'ctx-blocks')}
+							label={__('Circular image', 'ctx-blocks')}
 							checked={round}
 							onChange={(event) => {setAttributes( { round: event })}}
+							disabled={ image.subtype == 'svg+xml'}
 					/>
 					<CheckboxControl
 							label={__('Round corners', 'ctx-blocks')}
 							checked={roundCorners}
 							onChange={(event) => {setAttributes( { roundCorners: event })}}
+							disabled={round || image.subtype == 'svg+xml'}
 					/>
 					<CheckboxControl
 						label={__('Border', 'ctx-blocks')}
 						checked={border}
 						onChange={(event) => {setAttributes( { border: event })}}
+						disabled={image.subtype == 'svg+xml'}
 					/>
 					<CheckboxControl
 						label={__('Shadow', 'ctx-blocks')}
 						checked={shadow}
 						onChange={(event) => {setAttributes( { shadow: event })}}
+						disabled={image.subtype == 'svg+xml'}
 					/>
 					<CheckboxControl
 						label={__('Flip', 'ctx-blocks')}
 						checked={flip}
 						onChange={(event) => {setAttributes( { flip: event })}}
+						disabled={image.subtype == 'svg+xml'}
 					/>
 				</PanelBody>
 				<PanelBody
 					title={__('Behaviour', 'ctx-blocks')}
 					initialOpen={true}
 				>
-					<CheckboxControl
-						label={__("Lightbox", "ctx-blocks")}
-						help={__("Show image in fullscreen", 'ctx-blocks')}
-						value={hasLightbox}
-						onChange={(event) => {setAttributes( { hasLightbox: event })}}
-					/>                       
+					<SelectControl
+                            label={__('Action', 'ctx-blocks')}
+                            value={ hasLightbox ? 'modal' : 'link' }
+                            options={ [
+                                { label: __('Link', 'ctx-blocks'), value: 'link' },
+                                { label: __('Lightbox', 'ctx-blocks'), value: 'modal' },
+                            ] }
+                            onChange={ ( event ) => { setAttributes( { hasLightbox: event == "modal" } ) } }
+                    />
+					{ !hasLightbox && <BaseControl
+                            label={__("Add a URL or a link", 'ctx-blocks')}
+                        >
+                            <URLInput
+                                value={ url }
+                                onChange={ ( url, post ) => setAttributes( { url, text: (post && post.title) || __('Click here', 'ctx-blocks') } ) }
+                            />
+                        </BaseControl> }                   
 				</PanelBody>
 			</InspectorControls>
 		</>
