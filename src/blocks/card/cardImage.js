76aef679dc2b3086bab297de1b0aea115ac66e83
Thomas Gollenia <thomas@gollenia.at>
2022-05-23 08:06:04 +0200

    temp support for smaller inmages

--- src/blocks/card/cardImage.js
+++ src/blocks/card/cardImage.js
@@ -1,13 +1,14 @@
+import { __ } from '@wordpress/i18n'; 
+
 const CardImage = ({image, failMessage}) => {	
 	if (!image) return <></>;
 	
 	if (image.subtype == 'svg+xml') return (<img src={image.url}></img>);
 
-	if(!image?.sizes?.large) return(<p>{failMessage}</p>)
+	// TODO: Write a better mehtod to determine the best image size
+	if(!image?.sizes?.large) <><img src={image.sizes.medium.url}></img><p className="image__warning">{__('Image may be too small')}</p></>;
 	
-	return (
-		<img src={image.sizes.large.url}></img>
-	)
+	return (<img src={image.sizes.large.url}></img>);
 }
 
 export default CardImage;
