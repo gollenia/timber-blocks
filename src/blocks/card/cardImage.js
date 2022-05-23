2a20506ce81cea358c4ab3e50b7f332361456354
Thomas Gollenia <thomas@gollenia.at>
2022-05-23 08:12:27 +0200

    quick fix reverted

--- src/blocks/card/cardImage.js
+++ src/blocks/card/cardImage.js
@@ -1,14 +1,13 @@
-import { __ } from '@wordpress/i18n'; 
-
 const CardImage = ({image, failMessage}) => {	
 	if (!image) return <></>;
 	
 	if (image.subtype == 'svg+xml') return (<img src={image.url}></img>);
 
-	// TODO: Write a better mehtod to determine the best image size
-	if(!image?.sizes?.large) <><img src={image.sizes.medium.url}></img><p className="image__warning">{__('Image may be too small')}</p></>;
+	if(!image?.sizes?.large) return(<p>{failMessage}</p>)
 	
-	return (<img src={image.sizes.large.url}></img>);
+	return (
+		<img src={image.sizes.large.url}></img>
+	)
 }
 
 export default CardImage;
