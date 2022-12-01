39be0a14337856f6087c394f1b52adce229ef310
Thomas Gollenia <thomas@gollenia.at>
2022-12-01 08:58:49 +0100

    types added

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -3,3 +3,11 @@ declare module '@wordpress/block-editor';
 declare module '@wordpress/components';
 declare module '@wordpress/blocks';
 declare module '@wordpress/core-data';
+
+type BlockSettings = {
+	title: string;
+	icon: any;
+	description: string;
+	edit: () => any;
+	save: () => any;
+};
