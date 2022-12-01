493db260d34095b735d9686090470d6cc4286d9d
Thomas Gollenia <thomas@gollenia.at>
2022-12-01 09:03:54 +0100

    added customColor

--- src/blocks/card/edit.js
+++ src/blocks/card/edit.js
@@ -1,128 +1,163 @@
+import { useBlockProps, useInnerBlocksProps } from '@wordpress/block-editor';
+import { Icon } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import { colord } from 'colord';
 import Inspector from './inspector';
 import Toolbar from './toolbar';
-import { __ } from '@wordpress/i18n'; 
-import { useBlockProps, InnerBlocks, useInnerBlocksProps } from '@wordpress/block-editor';
-import { colord } from 'colord';
-import { Icon } from '@wordpress/components'
 
-import CardImage  from './cardImage';
+import CardImage from './cardImage';
 import icons from './icons';
 
-export default function CardEdit({...props}) {
+export default function CardEdit( { ...props } ) {
+	const {
+		attributes: {
+			imagePosition,
+			textAlign,
+			isFirst,
+			isLast,
+			label,
+			badge,
+			hover,
+			image,
+			url,
+			customBackgroundColor,
+			customSecondaryColor,
+		},
 
-		const {
-			attributes: {
-				imagePosition,
-				textAlign,
-				isFirst,
-				isLast,
-                label,
-                badge,
-				hover,
-				image,
-				url
+		backgroundColor,
+		secondaryColor,
+	} = props;
+
+	const allowedBlocks = [
+		'core/spacer',
+		'core/separator',
+		'core/paragraph',
+		'core/shortcode',
+		'core/heading',
+		'core/list',
+		'ctx-blocks/button-group',
+		'ctx-blocks/button',
+		'ctx-blocks/image',
+		'ctx-blocks/nav',
+		'ctx-blocks/posts',
+		'ctx-blocks/grid-row',
+		'ctx-blocks/description-list',
+		'ctx-blocks/accordion-collection',
+		'ctx-blocks/progress',
+		'events-manager/details',
+		'events-manager/booking',
+	];
+
+	const template = [
+		[
+			'core/heading',
+			{
+				placeholder: __( 'Title', 'ctx-blocks' ),
+				className: 'card__title',
+				level: 2,
 			},
-			backgroundColor,
-            secondaryColor
-		} = props;
+		],
+		[
+			'core/heading',
+			{
+				placeholder: __( 'Subtitle', 'ctx-blocks' ),
+				className: 'card__subtitle',
+				level: 4,
+			},
+		],
+		[
+			'core/paragraph',
+			{
+				placeholder: __( 'Your content goes here...', 'ctx-blocks' ),
+				className: 'card__text',
+			},
+		],
+	];
 
-		const allowedBlocks = [
-			'core/spacer', 
-			'core/separator', 
-			'core/paragraph', 
-			'core/shortcode', 
-			'core/heading', 
-			'core/list', 
-			'ctx-blocks/button-group', 
-			'ctx-blocks/button',  
-			'ctx-blocks/image', 
-			'ctx-blocks/nav', 
-			'ctx-blocks/posts', 
-			'ctx-blocks/grid-row', 
-			'ctx-blocks/description-list', 
-			'ctx-blocks/accordion-collection', 
-			'ctx-blocks/progress',
-			'events-manager/details',
-			'events-manager/booking'
-		]
+	console.log( {
+		bc: backgroundColor,
+		cc: customBackgroundColor,
+	} );
 
-		const template = [
-			[
-				'core/heading', 
-				{
-					placeholder: __('Title', 'ctx-blocks'),
-					className: 'card__title',
-					level: 2
-				}
-			],
-			[
-				'core/heading', 
-				{
-					placeholder: __('Subtitle', 'ctx-blocks'),
-					className: 'card__subtitle',
-					level: 4
-				}
-			],
-			[
-				'core/paragraph',
-				{
-					placeholder: __('Your content goes here...', 'ctx-blocks'),
-					className: 'card__text'
-				}
-			]
-		];
-	
-		const style = {
-			background: backgroundColor.color, 
-			color: colord(backgroundColor.color).isDark() ? "#ffffff" : "#000000"
-		};
+	const getSecondaryStyle = () => {
+		let bgValue = 'var(--primary)';
+		let fbValue = 'var(--primary-contrast)';
+		ifcustomSecondaryColor ?? backgroundColor.color
+	}
 
-        const secondaryStyle = {
-			background: secondaryColor.color, 
-			color: colord(secondaryColor.color).isDark() ? "#ffffff" : "#000000"
-		};
+	const backgroundColorValue = customBackgroundColor
+		? customBackgroundColor
+		: backgroundColor.color ?? '#ffffff';
 
-		const innerBlockProps = useInnerBlocksProps({}, {allowedBlocks, template})
+	const secondaryColorValue = customSecondaryColor
+		? customSecondaryColor
+		: secondaryColor.color ?? 'var(--primary)';
 
-		const classes = [
-			"ctx:card",
-			isFirst ? "ctx:card__first" : false,
-			isLast ? "ctx:card__last" : false,
-			hover ? "ctx:card__hover" : false,
-			`ctx:card--${textAlign}`,
-			`ctx:card__image--${imagePosition}`
-		].filter(Boolean).join(" ");
+	const style = {
+		background: backgroundColorValue,
+		color: colord( backgroundColorValue ).isDark() ? '#ffffff' : '#000000',
+	};
 
-		console.log(icons)
-		return (
-			<>
-				<Inspector
-						{ ...props }
-				/>
-				<Toolbar
-					{ ...props }
-				/>
-				<div {...useBlockProps() } >
-					<div className="ctx:control__label ctx:card__label">
-						<label>{__('Card', 'ctx-blocks')}</label>
-						<div className="ctx:control__icons">
-						{ url != '' && <Icon className="ctx:control__icon" icon={icons?.url} size={18}/> }
-						</div>
+	const secondaryStyle = {
+		background: secondaryColorValue,
+		color: colord( secondaryColorValue ).isDark() ? '#ffffff' : '#000000',
+	};
+
+	const innerBlockProps = useInnerBlocksProps(
+		{},
+		{ allowedBlocks, template }
+	);
+
+	const classes = [
+		'ctx:card',
+		isFirst ? 'ctx:card__first' : false,
+		isLast ? 'ctx:card__last' : false,
+		hover ? 'ctx:card__hover' : false,
+		`ctx:card--${ textAlign }`,
+		`ctx:card__image--${ imagePosition }`,
+	]
+		.filter( Boolean )
+		.join( ' ' );
+
+	console.log( icons );
+	console.log( customBackgroundColor );
+	return (
+		<>
+			<Inspector { ...props } />
+			<Toolbar { ...props } />
+			<div { ...useBlockProps() }>
+				<div className="ctx:control__label ctx:card__label">
+					<label>{ __( 'Card', 'ctx-blocks' ) }</label>
+					<div className="ctx:control__icons">
+						{ url != '' && (
+							<Icon
+								className="ctx:control__icon"
+								icon={ icons?.url }
+								size={ 18 }
+							/>
+						) }
 					</div>
-					<div style={style} className={classes}>
-                        { badge != "" && <b className="ctx:card__badge" style={secondaryStyle}><b>{badge}</b></b> }
-						<CardImage 
-							image={image}
-							failMessage={__('Image is too small. Please upload a larger version')}
-						/>
-                        <div className="ctx:card__content">
-                            { label != '' && <label style={secondaryStyle}>{label}</label> }
-							<div {...innerBlockProps}></div>
-                        </div>
-					</div>
-					
 				</div>
-			</>
-		);
-
+				<div style={ style } className={ classes }>
+					{ badge != '' && (
+						<b className="ctx:card__badge" style={ secondaryStyle }>
+							<b>{ badge }</b>
+						</b>
+					) }
+					<CardImage
+						image={ image }
+						failMessage={ __(
+							'Image is too small. Please upload a larger version'
+						) }
+					/>
+					<div className="ctx:card__content">
+						{ label != '' && (
+							<label style={ secondaryStyle }>{ label }</label>
+						) }
+						<div { ...innerBlockProps }></div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
--- src/blocks/card/inspector.js
+++ src/blocks/card/inspector.js
@@ -1,12 +1,24 @@
+import {
+	InspectorControls,
+	MediaUpload,
+	MediaUploadCheck,
+	PanelColorSettings,
+	URLInput,
+} from '@wordpress/block-editor';
+import {
+	BaseControl,
+	Button,
+	Icon,
+	PanelBody,
+	PanelRow,
+	TextControl,
+	ToggleControl,
+} from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
-import { URLInput, MediaUpload, MediaUploadCheck, InspectorControls } from '@wordpress/block-editor';
-import { BaseControl, PanelBody, PanelRow, ToggleControl, TextControl } from '@wordpress/components';
-import { Icon, Button} from '@wordpress/components'
 import { has } from 'lodash';
-import { PanelColorSettings } from '@wordpress/block-editor';
-import icons from './icons.js'
+import icons from './icons.js';
 
-const Inspector = (props) => {
+const Inspector = ( props ) => {
 	const {
 		attributes: {
 			image,
@@ -19,7 +31,9 @@ const Inspector = (props) => {
 			badge,
 			isFirst,
 			isLast,
-			actions
+			actions,
+			customBackgroundColor,
+			customSecondaryColor,
 		},
 		setAttributes,
 		backgroundColor,
@@ -28,157 +42,276 @@ const Inspector = (props) => {
 		setSecondaryColor,
 	} = props;
 
-
-	const isSVG = ( image != null ? image.subtype == "svg+xml" : false );
-	const imageUrl = image == null ? false : (!(has(image, 'sizes.medium')) || isSVG ? image.url : image.sizes.medium.url);
+	const isSVG = image != null ? image.subtype == 'svg+xml' : false;
+	const imageUrl =
+		image == null
+			? false
+			: ! has( image, 'sizes.medium' ) || isSVG
+			? image.url
+			: image.sizes.medium.url;
 
 	return (
-		
 		<InspectorControls>
-
 			<PanelBody
-				title={__('Appearance', 'ctx-blocks')}
-				initialOpen={false}
+				title={ __( 'Appearance', 'ctx-blocks' ) }
+				initialOpen={ false }
 			>
-				
-				
 				<ToggleControl
-					label={__('Shadow', 'ctx-blocks')}
-					checked={shadow}
-					onChange={(value) => {setAttributes( { shadow: value })}}
+					label={ __( 'Shadow', 'ctx-blocks' ) }
+					checked={ shadow }
+					onChange={ ( value ) => {
+						setAttributes( { shadow: value } );
+					} }
 				/>
 				<ToggleControl
-					label={__("Transparent", 'ctx-blocks')}
-					checked={transparent}
-					onChange={(value) => {setAttributes( { transparent: value })}}
+					label={ __( 'Transparent', 'ctx-blocks' ) }
+					checked={ transparent }
+					onChange={ ( value ) => {
+						setAttributes( { transparent: value } );
+					} }
 				/>
-				
+
 				<TextControl
-					label={__('Label', 'ctx-blocks')}
-					value={label}
-					onChange={(value) => {setAttributes( { label: value })}}
-					help={__('Adds a floating label for e.g. a category', 'ctx-blocks')}
+					label={ __( 'Label', 'ctx-blocks' ) }
+					value={ label }
+					onChange={ ( value ) => {
+						setAttributes( { label: value } );
+					} }
+					help={ __(
+						'Adds a floating label for e.g. a category',
+						'ctx-blocks'
+					) }
 				/>
 				<TextControl
-					label={__('Badge', 'ctx-blocks')}
-					value={badge}
-					onChange={(value) => {setAttributes( { badge: value })}}
-					help={__('Draw attention to this card or add a counter with a badge in the edge', 'ctx-blocks')}
+					label={ __( 'Badge', 'ctx-blocks' ) }
+					value={ badge }
+					onChange={ ( value ) => {
+						setAttributes( { badge: value } );
+					} }
+					help={ __(
+						'Draw attention to this card or add a counter with a badge in the edge',
+						'ctx-blocks'
+					) }
 				/>
-				</PanelBody>
+			</PanelBody>
 
 			<PanelColorSettings
-				title={__('Color settings', 'ctx-blocks')}
-				initialOpen={false}
-				colorSettings={[
+				title={ __( 'Color settings', 'ctx-blocks' ) }
+				initialOpen={ false }
+				colorSettings={ [
 					{
-						label: __('Background color', 'ctx-blocks'),
-						onChange: setBackgroundColor ,
-						value: backgroundColor.color,
-						disableCustomColors: true,
+						label: __( 'Background color', 'ctx-blocks' ),
+						onChange: setBackgroundColor,
+						value: customBackgroundColor ?? backgroundColor.color,
+						disableCustomColors: false,
+						defaultPalette: false,
 					},
 					{
-						label: __('Secondary color', 'ctx-blocks'),
-						onChange: setSecondaryColor ,
-						value: secondaryColor.color,
-						disableCustomColors: true,
-					}
-				]}
+						label: __( 'Secondary color', 'ctx-blocks' ),
+						onChange: setSecondaryColor,
+						value: customSecondaryColor ?? secondaryColor.color,
+						disableCustomColors: false,
+						defaultPalette: false,
+					},
+				] }
 			/>
-				
+
 			<PanelBody
-				title={__('Image', 'ctx-blocks')}
-				initialOpen={false}
+				title={ __( 'Image', 'ctx-blocks' ) }
+				initialOpen={ false }
 			>
 				<MediaUploadCheck>
-				<MediaUpload
-						onSelect={ ( media ) => setAttributes({image: media}) }
-						label={__('Image', 'ctx-blocks')}
-						value= { image }
+					<MediaUpload
+						onSelect={ ( media ) =>
+							setAttributes( { image: media } )
+						}
+						label={ __( 'Image', 'ctx-blocks' ) }
+						value={ image }
 						render={ ( { open } ) => {
-							return <div className="editor-post-featured-image ctx-image-select">
-								{ !image && <button type="button" className="components-button editor-post-featured-image__toggle" onClick={ open }>{__('Choose image', 'ctx-blocks')}</button> }
-								{ image && 
-									<div>
-										<img className="" src={imageUrl} onClick={open} alt={__("No image loaded", 'ctx-blocks')}/>
-										<PanelRow>
-										<Button type="button" isSecondary  onClick={ open }>{__("Replace image", 'ctx-blocks')}</Button>
-										<Button type="button" isDestructive  onClick={ () => setAttributes({ image: null }) }> {__("Remove image", 'ctx-blocks')}</Button>
-										</PanelRow>
-									</div> }
-							</div> ;
-							} }
+							return (
+								<div className="editor-post-featured-image ctx-image-select">
+									{ ! image && (
+										<button
+											type="button"
+											className="components-button editor-post-featured-image__toggle"
+											onClick={ open }
+										>
+											{ __(
+												'Choose image',
+												'ctx-blocks'
+											) }
+										</button>
+									) }
+									{ image && (
+										<div>
+											<img
+												className=""
+												src={ imageUrl }
+												onClick={ open }
+												alt={ __(
+													'No image loaded',
+													'ctx-blocks'
+												) }
+											/>
+											<PanelRow>
+												<Button
+													type="button"
+													isSecondary
+													onClick={ open }
+												>
+													{ __(
+														'Replace image',
+														'ctx-blocks'
+													) }
+												</Button>
+												<Button
+													type="button"
+													isDestructive
+													onClick={ () =>
+														setAttributes( {
+															image: null,
+														} )
+													}
+												>
+													{ ' ' }
+													{ __(
+														'Remove image',
+														'ctx-blocks'
+													) }
+												</Button>
+											</PanelRow>
+										</div>
+									) }
+								</div>
+							);
+						} }
 					/>
-					</MediaUploadCheck>
-					<PanelRow>
+				</MediaUploadCheck>
+				<PanelRow>
 					<div>
-					<label className="components-base-control__label" htmlFor="inspector-range-control-4">{__("Image position", 'ctx-blocks')}</label>
-					<div className="imagePositionSelector">
-							<Button onClick={ () => setAttributes({ imagePosition: "top" }) } className={imagePosition == "top" ? "active" : ""}>
-								<Icon size="64" className="icon" icon={icons.topimage}/>
-								<div>{__("top", 'ctx-blocks')}</div>
+						<label
+							className="components-base-control__label"
+							htmlFor="inspector-range-control-4"
+						>
+							{ __( 'Image position', 'ctx-blocks' ) }
+						</label>
+						<div className="imagePositionSelector">
+							<Button
+								onClick={ () =>
+									setAttributes( { imagePosition: 'top' } )
+								}
+								className={
+									imagePosition == 'top' ? 'active' : ''
+								}
+							>
+								<Icon
+									size="64"
+									className="icon"
+									icon={ icons.topimage }
+								/>
+								<div>{ __( 'top', 'ctx-blocks' ) }</div>
 							</Button>
-							<Button onClick={ () => setAttributes({ imagePosition: "bottom" }) } className={imagePosition == "bottom" ? "active" : ""}>
-								<Icon size="64" className="icon" icon={icons.bottomimage}/>
-								<div>{__("bottom", 'ctx-blocks')}</div>
+							<Button
+								onClick={ () =>
+									setAttributes( { imagePosition: 'bottom' } )
+								}
+								className={
+									imagePosition == 'bottom' ? 'active' : ''
+								}
+							>
+								<Icon
+									size="64"
+									className="icon"
+									icon={ icons.bottomimage }
+								/>
+								<div>{ __( 'bottom', 'ctx-blocks' ) }</div>
 							</Button>
-							<Button onClick={ () => setAttributes({ imagePosition: "left" }) } className={imagePosition == "left" ? "active" : ""}>
-								<Icon size="64" className="icon" icon={icons.leftimage}/>
-								<div>{__("left", 'ctx-blocks')}</div>
+							<Button
+								onClick={ () =>
+									setAttributes( { imagePosition: 'left' } )
+								}
+								className={
+									imagePosition == 'left' ? 'active' : ''
+								}
+							>
+								<Icon
+									size="64"
+									className="icon"
+									icon={ icons.leftimage }
+								/>
+								<div>{ __( 'left', 'ctx-blocks' ) }</div>
 							</Button>
-							<Button onClick={ () => setAttributes({ imagePosition: "right" }) } className={imagePosition == "right" ? "active" : ""}>
-								<Icon size="64" className="icon" icon={icons.rightimage}/>
-								<div>{__("right", 'ctx-blocks')}</div>
+							<Button
+								onClick={ () =>
+									setAttributes( { imagePosition: 'right' } )
+								}
+								className={
+									imagePosition == 'right' ? 'active' : ''
+								}
+							>
+								<Icon
+									size="64"
+									className="icon"
+									icon={ icons.rightimage }
+								/>
+								<div>{ __( 'right', 'ctx-blocks' ) }</div>
 							</Button>
-					</div>
+						</div>
 					</div>
 				</PanelRow>
 			</PanelBody>
 			<PanelBody
-				title={__("Behaviour", 'ctx-blocks')}
-				initialOpen={false}
+				title={ __( 'Behaviour', 'ctx-blocks' ) }
+				initialOpen={ false }
 			>
 				<PanelRow>
 					<ToggleControl
-						label={__('Highlight on mouse over', 'ctx-blocks')}
-						checked={hover}
-						onChange={(value) => {setAttributes( { hover: value })}}
+						label={ __( 'Highlight on mouse over', 'ctx-blocks' ) }
+						checked={ hover }
+						onChange={ ( value ) => {
+							setAttributes( { hover: value } );
+						} }
 					/>
-					
 				</PanelRow>
-				<BaseControl
-						label={__('Link', 'ctx-blocks')}
-					>
-						<URLInput
-							value={ url }
-							onChange={ ( url, post ) => setAttributes( { url, text: (post && post.title) || __('Click here...', 'ctx-blocks') } ) }
-						/>
-					</BaseControl>
+				<BaseControl label={ __( 'Link', 'ctx-blocks' ) }>
+					<URLInput
+						value={ url }
+						onChange={ ( url, post ) =>
+							setAttributes( {
+								url,
+								text:
+									( post && post.title ) ||
+									__( 'Click here...', 'ctx-blocks' ),
+							} )
+						}
+					/>
+				</BaseControl>
 			</PanelBody>
 			<PanelBody
-				title={__("Layout settings", 'ctx-blocks')}
-				initialOpen={false}
+				title={ __( 'Layout settings', 'ctx-blocks' ) }
+				initialOpen={ false }
 			>
 				<PanelRow>
 					<ToggleControl
-						label={__("Is first element", 'ctx-blocks')}
-						checked={isFirst}
-						onChange={(value) => {setAttributes( { isFirst: value })}}
-					/>  
+						label={ __( 'Is first element', 'ctx-blocks' ) }
+						checked={ isFirst }
+						onChange={ ( value ) => {
+							setAttributes( { isFirst: value } );
+						} }
+					/>
 				</PanelRow>
 				<PanelRow>
 					<ToggleControl
-						label={__("Is last element", 'ctx-blocks')}
-						checked={isLast}
-						onChange={(value) => {setAttributes( { isLast: value })}}
+						label={ __( 'Is last element', 'ctx-blocks' ) }
+						checked={ isLast }
+						onChange={ ( value ) => {
+							setAttributes( { isLast: value } );
+						} }
 					/>
 				</PanelRow>
-
 			</PanelBody>
 		</InspectorControls>
-
 	);
-	
-}
+};
 
 export default Inspector;
