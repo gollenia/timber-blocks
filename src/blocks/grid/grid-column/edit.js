4fd2c253c18b3496c8846a53a19ad5ab0c0d97cf
Thomas Gollenia <thomas@gollenia.at>
2023-04-09 07:03:54 +0200

    some changes

--- src/blocks/grid/grid-column/edit.js
+++ src/blocks/grid/grid-column/edit.js
@@ -1,36 +1,37 @@
 import Inspector from './inspector';
 
-import { __ } from '@wordpress/i18n'; 
-import { useBlockProps, useInnerBlocksProps} from '@wordpress/block-editor';
+import { useBlockProps, useInnerBlocksProps } from '@wordpress/block-editor';
+import { __ } from '@wordpress/i18n';
 
+export default function GridColumnEdit( { ...props } ) {
+	const {
+		attributes: { widthLarge },
+		className,
+	} = props;
 
-export default function GridColumnEdit ({...props}) {
+	const TEMPLATE = [ [ 'core/paragraph', {} ] ];
 
-		const {
-			attributes: { widthLarge },
-		} = props;
+	const size = widthLarge == 0 ? 'Auto' : widthLarge;
 
-		const TEMPLATE = [ [ 'core/paragraph', {} ] ];
+	const blockProps = useBlockProps( {
+		className: className + ` ctx:column ctx:column--${ widthLarge }`,
+	} );
 
-		const size = widthLarge == 0 ? "Auto" : widthLarge;
+	const innerBlocksProps = useInnerBlocksProps( blockProps, {
+		template: TEMPLATE,
+	} );
 
-		const blockProps = useBlockProps({className: `ctx:column ctx:column--${widthLarge}`})
-
-		const innerBlocksProps = useInnerBlocksProps({ className: 'ctx:column__wrapper' }, { template: TEMPLATE });
-
-
-		return (
-			<>
-				<Inspector
-						{ ...props }
-				/>
-				<div {...blockProps}>
-                    <div className="ctx:column__label ctx:control__label">
-                        <label>{__('Column (width: ', 'ctx-blocks') + size + ')'}</label>
-                    </div>
-					<div {...innerBlocksProps}></div>			
+	return (
+		<>
+			<Inspector { ...props } />
+			<div { ...blockProps }>
+				<div className="ctx:column__label ctx:control__label">
+					<label>
+						{ __( 'Column (width: ', 'ctx-blocks' ) + size + ')' }
+					</label>
 				</div>
-				
-			</>
-		);
+				<div { ...innerBlocksProps }></div>
+			</div>
+		</>
+	);
 }
