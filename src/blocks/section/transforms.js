e34dd903649bb3e9de6641244d0cd842dad3ac2b
Thomas Gollenia <thomas@gollenia.at>
2023-05-12 08:55:13 +0200

    correct transfoprmation to group

---
+++ src/blocks/section/transforms.js
@@ -0,0 +1,16 @@
+import { createBlock } from '@wordpress/blocks';
+
+const transforms = {
+	to: [
+		{
+			type: 'block',
+			blocks: [ 'core/group' ],
+			transform: ( attributes, innerBlocks ) => {
+				console.log( innerBlocks );
+				return createBlock( 'core/group', attributes, innerBlocks );
+			},
+		},
+	],
+};
+
+export default transforms;
