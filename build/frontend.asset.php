138091fae471abd386525751254aeaf586604d36
Thomas Gollenia <thomas@gollenia.at>
2023-11-30 17:21:22 +0100

    remove build

--- build/frontend.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '12d3b42dada5c0c5e929');
--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '847cd8ffe0cc0814c878');
