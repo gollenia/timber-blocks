aba4080af3342952945ff31905abba76c06c2d53
Thomas Gollenia <thomas@gollenia.at>
2023-12-01 17:45:08 +0100

    transformation

--- src/blocks/image/transforms.js
+++ src/blocks/image/transforms.js
@@ -6,6 +6,16 @@ const transforms = {
 			type: 'block',
 			blocks: ['core/image'],
 			transform: (attributes, innerBlocks) => {
+				attributes.id = attributes.image.id;
+				attributes.href = attributes.url;
+				attributes.url = attributes.image.url;
+				attributes.alt = attributes.image.alt;
+				attributes.title = attributes.image.title;
+
+				attributes.width = attributes.width + '%';
+
+				attributes.image = undefined;
+
 				return createBlock('core/image', attributes, innerBlocks);
 			},
 		},
