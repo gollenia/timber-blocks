e3433693ab7d83fc1e25a40bdd1513aa578a604f
Thomas Gollenia <thomas@gollenia.at>
2024-02-06 10:44:12 +0100

    new block icon

--- src/blocks/grid-row/icons.js
+++ src/blocks/grid-row/icons.js
@@ -3,15 +3,14 @@ import { Path, SVG } from '@wordpress/components';
 let icons = {};
 
 icons.row = (
-	<SVG
+	<svg
 		xmlns="http://www.w3.org/2000/svg"
 		height="24"
-		viewBox="0 0 24 24"
+		viewBox="0 -960 960 960"
 		width="24"
 	>
-		<path d="M0 0h24v24H0z" fill="none" />
-		<Path d="M22 13h-8v-2h8v2zm0-6h-8v2h8V7zm-8 10h8v-2h-8v2zm-2-8v6c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V9c0-1.1.9-2 2-2h6c1.1 0 2 .9 2 2zm-1.5 6l-2.25-3-1.75 2.26-1.25-1.51L3.5 15h7z" />
-	</SVG>
+		<path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm560-200H200v120h560v-120Zm-100-80h100v-360H660v360Zm-460 0h100v-360H200v360Zm180 0h200v-360H380v360Z" />
+	</svg>
 );
 icons.parallax = (
 	<SVG
