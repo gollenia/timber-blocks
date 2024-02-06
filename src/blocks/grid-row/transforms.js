e4ccc912b50707c4858046be08b42d40d8005786
Thomas Gollenia <thomas@gollenia.at>
2024-02-06 10:58:27 +0100

    transfoprms column width

--- src/blocks/grid-row/transforms.js
+++ src/blocks/grid-row/transforms.js
@@ -1,5 +1,9 @@
 import { createBlock } from '@wordpress/blocks';
 
+const getWidthInPercent = (columnCount, columnSpan) => {
+	return (columnSpan / columnCount) * 100;
+};
+
 const transforms = {
 	to: [
 		{
@@ -12,8 +16,21 @@ const transforms = {
 
 				const newInnerBlocks = [];
 				innerBlocks.forEach((block) => {
+					const newWidth = getWidthInPercent(
+						attributes.childrenWidthLarge,
+						block.attributes.widthLarge !== 0
+							? block.attributes.widthLarge
+							: 1
+					);
+
 					newInnerBlocks.push(
-						createBlock('core/column', {}, block.innerBlocks)
+						createBlock(
+							'core/column',
+							{
+								width: newWidth,
+							},
+							block.innerBlocks
+						)
 					);
 				});
 
